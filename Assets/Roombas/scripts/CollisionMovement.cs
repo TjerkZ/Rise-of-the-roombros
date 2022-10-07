052aeeaad10673c133c1ac0afe10ddbf318bb0c5
BurlingtonApps <lala@pepr.nl>
2022-10-07 09:04:42 +0200

    rotate on collision

--- Assets/Roombas/scripts/CollisionMovement.cs
+++ Assets/Roombas/scripts/CollisionMovement.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class CollisionMovement : MonoBehaviour
 {
-    [SerializeField] float rotationspeed = 20;
-    [SerializeField][Range(0f,360f)] float direction = 0f;
-    private Vector3 RotateTo = new Vector3(0, -45, 0);
+    [SerializeField] float RotationSpeed = 20;
+    [SerializeField] float MoveSpeed = 2;
+    [SerializeField] float chaseRange = 10f;
+    [SerializeField][Range(0f,359f)] float direction = 0f;
+    [SerializeField][Range(0f, 45f)] float MinAngle = 40f;
+    [SerializeField][Range(46f, 90f)] float MaxAngle = 70f;
+
+    private Vector3 RotateTo;
     private bool Rotating = true;
     private bool TuringRight = true;
+    private float distanceToTarget = Mathf.Infinity;
 
     // Start is called before the first frame update
     void Start()
     {
         TuringRight = (Random.value > 0.5f);
         RotateTo = GetNewAngle();
+        //set object to move direction
+        transform.Rotate(new Vector3(0, direction, 0));
     }
 
     // Update is called once per frame
@@ -34,13 +42,21 @@ public class CollisionMovement : MonoBehaviour
         
         if (collision.gameObject.tag == "RoombaBorder")
         {
-            Debug.Log("Hit Object");
+            Debug.Log("rotating");
+            RotateTo = GetNewAngle();
+            Rotating = true;
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 
     void MoveForward()
     {
-        transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime);
     }
 
     Vector3 GetNewAngle()
@@ -48,13 +64,16 @@ public class CollisionMovement : MonoBehaviour
         //switch direction everytime function is called
         TuringRight = !TuringRight;
 
-        float randomRange = Random.Range(direction + 40f, direction + 70f);
+        float randomRange = Random.Range(MinAngle, MaxAngle);
+        Debug.Log(randomRange);
+        Debug.Log(ConvertToAngle(randomRange));
 
         return new Vector3(0, ConvertToAngle(randomRange), 0);
     }
 
     private float ConvertToAngle(float angle)
     {
+        //calulate angle based on direction
         if (TuringRight)
         {
             angle = direction + angle;
@@ -64,7 +83,7 @@ public class CollisionMovement : MonoBehaviour
             angle = direction - angle;
         }
 
-
+        //convert the angel over 360 and under 0 to stay in 360 range
         if (angle >= 360)
         {
             angle -= 360;
@@ -84,10 +103,10 @@ public class CollisionMovement : MonoBehaviour
 
         if (!(transform.eulerAngles.y > RotateTo.y && transform.eulerAngles.y < RotateTo.y + 1f) && TuringRight)
         {
-            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotationspeed);
+            transform.Rotate(RotationSpeed * Time.deltaTime * new Vector3(0, 1, 0));
         }else if (!(transform.eulerAngles.y > RotateTo.y - 1 && transform.eulerAngles.y < RotateTo.y) && !TuringRight)
         {
-            transform.Rotate(rotationspeed * Time.deltaTime * new Vector3(0, -1, 0));
+            transform.Rotate(RotationSpeed * Time.deltaTime * new Vector3(0, -1, 0));
         }else{
             Rotating = false;
         }
